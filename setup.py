e6ef14fb865597e7eda7f2b8bed4c1354d169d54
2000game <felix.kotschenreuther@gmail.com>
2022-04-28 22:21:05 +0200

    changed version tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pysignageapi',
-    version='1.0.7',
+    version='1.0.8',
     packages=['pysignageserver'],
     url='https://github.com/2000game/pysignageapi',
     license='',

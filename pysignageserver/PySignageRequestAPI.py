3c7c0556d5d4bf329fd40acd0b556a60cf522292
2000game <felix.kotschenreuther@gmail.com>
2022-04-30 14:21:43 +0200

    fixed get_playlist_state bug

--- pysignageserver/PySignageRequestAPI.py
+++ pysignageserver/PySignageRequestAPI.py
@@ -7,7 +7,7 @@ class PySignageAPI():
 
     def post_call(self, datapoint, body=None):
         try:
-            r = requests.post(self.host + datapoint, data=body, timeout=5)
+            r = requests.post(self.host + datapoint, data=body, timeout=20)
             if r.status_code == 200:
                 return SUCCESS
         except TypeError:
@@ -17,7 +17,7 @@ class PySignageAPI():
 
     def get_call(self, datapoint):
         try:
-            r = requests.get(self.host + datapoint, timeout=5)
+            r = requests.get(self.host + datapoint, timeout=20)
             if r.status_code == 200:
                 return self._string_to_json(r.text)
         except TypeError:

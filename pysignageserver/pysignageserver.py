b2b0a31632ae9ac2d9ae13cc31f3f16bff0ca7d9
2000game <felix.kotschenreuther@gmail.com>
2022-05-01 09:16:37 +0200

    changed version

--- pysignageserver/pysignageserver.py
+++ pysignageserver/pysignageserver.py
@@ -241,7 +241,8 @@ class PySignageServer(PySignageAPI):
         return self.playlists
 
     def get_playlist_state(self, playlist_id):
-        for device in self.device_dict.values():
+        devices = self.device_dict.values()
+        for device in devices:
             device_playlist = device['player_class'].get_active_playlist()
             if device_playlist == playlist_id:
                 return True
